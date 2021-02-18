783c19c7ceaf4cd8f7b6597c3ee89a5d67b54a23
gowthamchandu <77114064+gowthamchandu@users.noreply.github.com>
2021-02-18 22:42:41 +0530

    Add files via upload

---
+++ src/main/java/com/example/SonarQubeGradleDemo/SonarQubeGradleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SonarQubeGradleDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarQubeGradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarQubeGradleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SonarQubeGradleDemo/SonarQubeGradleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SonarQubeGradleDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarQubeGradleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
